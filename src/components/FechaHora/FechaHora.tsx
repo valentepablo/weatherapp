7d29c9caea4c28e59cc135075b05e592debd7c17
Pablo Valente <pablo.nvalente@gmail.com>
2022-11-22 19:43:52 -0300

    fix spacing

--- src/components/FechaHora/FechaHora.tsx
+++ src/components/FechaHora/FechaHora.tsx
@@ -1,6 +1,6 @@
 const FechaHora = () => {
   return (
-    <div className='mt-10 md:mt-20 text-center text-violet-400'>Noviembre 22, 2022 - 6:26 PM</div>
+    <div className='mt-2 md:mt-20 text-center text-violet-400'>Noviembre 22, 2022 - 6:26 PM</div>
   );
 };
 
--- src/components/ResultContainer/ResultadoClima.tsx
+++ src/components/ResultContainer/ResultadoClima.tsx
@@ -1,12 +1,12 @@
 const ResultadoClima = () => {
   return (
-    <div className='p-3 rounded-lg text-center '>
+    <div className=' rounded-lg text-center '>
       <p className='text-3xl font-bold uppercase tracking-wider text-violet-300'>Buenos Aires</p>
-      <p className='text-9xl font-semibold mt-2 relative'>
+      <p className='text-9xl font-semibold relative'>
         26
         <span className='absolute top-4 text-4xl'>&#8451;</span>
       </p>
-      <div className='mt-8 text-violet-300 grid place-items-center'>
+      <div className='mt-2 text-violet-300 grid place-items-center'>
         <div className='flex items-center gap-2'>
           <p>ST</p>
           <p className='font-semibold'>28&#8451;</p>
