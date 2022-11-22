61d9eb58bb37020dac67c455f2763dba738eb139
Pablo Valente <pablo.nvalente@gmail.com>
2022-11-22 20:03:48 -0300

    fix 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ResultContainer from './components/ResultContainer/ResultContainer';
 
 function App() {
   return (
-    <div className='py-3 flex flex-col'>
+    <div className='py-3 md:py-10 h-screen flex flex-col justify-between'>
       <InputField />
       <ResultContainer />
       <FechaHora />
