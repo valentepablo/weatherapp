4dfc7f86e36eaed4dc2651500f8b0b70e39e5239
Pablo Valente <pablo.nvalente@gmail.com>
2022-11-22 19:33:14 -0300

    fix hora y fecha positioning

--- src/components/FechaHora/FechaHora.tsx
+++ src/components/FechaHora/FechaHora.tsx
@@ -1,8 +1,6 @@
 const FechaHora = () => {
   return (
-    <div className='fixed bottom-10 inset-x-3 text-center text-violet-400'>
-      Noviembre 22, 2022 - 6:26 PM
-    </div>
+    <div className='mt-10 md:mt-20 text-center text-violet-400'>Noviembre 22, 2022 - 6:26 PM</div>
   );
 };
 
